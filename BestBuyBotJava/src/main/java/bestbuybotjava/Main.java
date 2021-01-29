97e38b37ef59381730de09794aed108cb88db1af
CrispyyBaconx <immabaconback@gmail.com>
2021-01-28 21:22:16 -0500

    Did Shit
    yes

--- BestBuyBotJava/src/main/java/bestbuybotjava/Main.java
+++ BestBuyBotJava/src/main/java/bestbuybotjava/Main.java
@@ -25,7 +25,8 @@ public class Main {
         System.out.println();
 
         // variable init
-        String url;
+        String url = "";
+        String sold = "Sold Out!";
 
         while(true) { // test url
             if (testUrl.substring(testUrl.indexOf("www."), testUrl.indexOf(".com")).equals("www.bestbuy")) {
@@ -44,17 +45,17 @@ public class Main {
         WebDriverWait wait = new WebDriverWait(driver, 100);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[3]/main/div[2]/div[3]/div[2]/div/div/div[4]/div/div/h2")));
         formattedPrinter("Website fully loaded");
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         formattedPrinter("Attempting to add to cart...");
         while (true) {
             WebElement hopefullyAddToCart = driver.findElement(By.xpath("/html/body/div[3]/main/div[2]/div[3]/div[2]/div/div/div[7]/div[1]/div/div/div/button"));
+            //WebElement annoyingSurvey = driver.findElement(By.xpath(""));
             if (hopefullyAddToCart.getText().equals("Sold Out")) {
-                formattedPrinter("Sold Out!");
-                try {
-                    Thread.sleep(10000);
-                    driver.navigate().refresh();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                spinnyNotSold(driver, sold);
             }
             else{
                 formattedPrinter("In Stock!");
@@ -67,5 +68,24 @@ public class Main {
 
     public static void formattedPrinter(String string) {
         System.out.println(" *-*  " + string);
+    }
+
+    public static void spinnyNotSold(WebDriver driver, String sold) {
+        System.out.println(" */*  " + sold);
+        System.out.print("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");
+        System.out.println(" *-*  " + sold);
+        System.out.print("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");
+        System.out.println(" *\\*  " + sold);
+        System.out.print("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");
+        System.out.println(" *|*  " + sold);
+        System.out.print("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");
+        
+        try {
+            Thread.sleep(10000);
+            driver.navigate().refresh();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
