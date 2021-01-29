676d654c664c38491ce5bee290692227d8d7d278
CrispyyBaconx <immabaconback@gmail.com>
2021-01-28 19:41:15 -0500

    Added shit
    read the title

--- BestBuyBotJava/src/main/java/bestbuybotjava/Main.java
+++ BestBuyBotJava/src/main/java/bestbuybotjava/Main.java
@@ -1,13 +1,10 @@
 package bestbuybotjava;
 
-import java.util.concurrent.TimeUnit;
 import java.util.Scanner;
-import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 
@@ -41,8 +38,14 @@ public class Main {
         
         driver.get(url);
         formattedPrinter("Successfully connected to " + url);
+
+        WebDriverWait wait = new WebDriverWait(driver, 100);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[3]/main/div[2]/div[3]/div[2]/div/div/div[4]/div/div/h2")));
+        formattedPrinter("Website fully loaded");
+        formattedPrinter("");
+
         
-        
+
 
         inputStream.close();
     }
--- BestBuyBotJava/src/test/java/bestbuybotjava/Test.java
+++ BestBuyBotJava/src/test/java/bestbuybotjava/Test.java
@@ -1,14 +1,8 @@
 package bestbuybotjava;
 
-import java.util.concurrent.TimeUnit;
 import java.util.Scanner;
-import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class Test {
@@ -24,6 +18,7 @@ public class Test {
         System.out.println();
         */
 
+        driver.get("google.com");
         final String testUrl = "https://www.bestbuy.com/site/gigabyte-nvidia-geforce-gtx-1650-super-oc-edition-4gb-gddr6-pci-express-3-0-graphics-card-black-gray/6409188.p?skuId=6409188";
 
         System.out.println(testUrl.substring(testUrl.indexOf("www."), testUrl.indexOf(".com")).equals("www.bestbuy"));
